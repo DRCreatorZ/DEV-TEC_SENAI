4cd2c157a7b4b55748f91dc66f90e9170d3b0b09
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2023-08-22 19:40:07 -0300

    Aula 22/08/23

--- 2Semestre/FPOO/GUIInicio/src/JFrameSwing.java
+++ 2Semestre/FPOO/GUIInicio/src/JFrameSwing.java
@@ -1,3 +1,10 @@
-public class JFrameSwing {
-    
+import javax.swing.JFrame;
+
+public class JFrameSwing extends JFrame {
+    public JFrameSwing() {
+        super("Minha Primeira janela JFrameSwing");
+        this.setSize(300,300);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }
--- 2Semestre/PHP/exemploHeader/exemploHeader.php
+++ 2Semestre/PHP/exemploHeader/exemploHeader.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 $x = 1;
@@ -11,18 +10,4 @@ else:
 	header('Location: erro.php');
 endif;
 
-=======
-<?php
-
-$x = 1;
-
-//echo $x;
-
-if($x <= 5):
-	header('Location: sucesso.php');
-else:
-	header('Location: erro.php');
-endif;
-
->>>>>>> 07498f7f1c994d1723759421fc2cf39c13733e53
 ?>

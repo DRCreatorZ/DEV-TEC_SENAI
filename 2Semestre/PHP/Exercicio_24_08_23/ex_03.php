e300a28ffed50c0f2f657b02182b22b7bbb00d5e
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2023-08-28 21:58:59 -0300

    aula 28/08/23

--- 2Semestre/PHP/Exercicio_24_08_23/ex_03.php
+++ 2Semestre/PHP/Exercicio_24_08_23/ex_03.php
@@ -3,11 +3,13 @@
     $horasTrabalhadas = $_GET['horasTrabalhadas'];
 	
 	function contraCheque($salarioHora, $horasTrabalhadas){
-        $salarioBruto = 'Salário Bruto: ....R$' .($salarioHora * $horasTrabalhadas) .'<br>';
-        $iR = 'Imposto de Renda: ....R$' .(($salarioHora * $horasTrabalhadas) * (11/100)) .'<br>';
-        $inss = 'INSS: ....R$' .(($salarioHora * $horasTrabalhadas) * (8/100)) .'<br>';
-        $sindicato = 'Sindicato: ....R$' .(($salarioHora * $horasTrabalhadas) * (5/100)) .'<br>';
-        $salarioLiquido = 'Salário Líquido: ....R$' .(($salarioHora * $horasTrabalhadas)-($salarioHora * $horasTrabalhadas)*(24/100)).'<br>';
+        echo '****Folha de Pagamento****<br>';
+        $sB = ($salarioHora * $horasTrabalhadas);
+        $salarioBruto = 'Salário Bruto: ...........R$' .$sB.'<br>';
+        $iR = 'Imposto de Renda: ....R$' .$sB * (11/100) .'<br>';
+        $inss = 'INSS: ........................R$' .$sB * (8/100) .'<br>';
+        $sindicato = 'Sindicato: ..................R$' .$sB * (5/100) .'<br>';
+        $salarioLiquido = 'Salário Líquido: ........R$' .$sB-$sB*(24/100).'<br>';
 
         return $salarioBruto .$iR .$inss .$sindicato .$salarioLiquido;
 

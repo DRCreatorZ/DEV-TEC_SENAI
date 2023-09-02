eac6c7782d226ee017a2ed5638c2048142e8725b
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2023-09-01 21:59:27 -0300

    Aula 01/09/23

---
+++ 2Semestre/PWFE/SituacaoAprendizagem2/script.js
@@ -0,0 +1,46 @@
+const vermelho = document.querySelector('.vermelho');
+const amarelo = document.querySelector('.amarelo');
+const verde = document.querySelector('.verde');
+
+let contador = 1;
+if ( 1 == 1) {
+	setInterval(() => {
+		if (contador <= 1) {
+			vermelho.classList.add('vermelho-ativo');
+		} else if (contador === 5) {
+			amarelo.classList.add('amarelo-ativo');
+			vermelho.classList.remove('vermelho-ativo');
+		} else if (contador === 8) {
+			verde.classList.add('verde-ativo');
+			amarelo.classList.remove('amarelo-ativo');
+		} else if (contador === 15) {
+			verde.classList.remove('verde-ativo');
+			contador = 0;
+		}
+
+		contador += 1;
+	}, 1000);
+} else {
+	setInterval(() => {
+		if (contador <= 1) {
+			amarelo.classList.add('amarelo-ativo');
+		} else if (contador === 2) {
+			amarelo.classList.add('amarelo-ativo');
+			amarelo.classList.remove('amarelo-ativo');
+			contador = 0;
+		}
+
+		contador += 1;
+	}, 1000);
+
+}
+
+function time()
+{
+today=new Date();
+h=today.getHours();
+m=today.getMinutes();
+s=today.getSeconds();
+document.getElementById('txt').innerHTML=h+":"+m+":"+s;
+setTimeout('time()',1000);
+}

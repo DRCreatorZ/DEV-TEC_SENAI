eaccc11882d8c7bfc7634a10b2152f271623c833
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2023-09-13 20:24:40 -0300

    Aula 13/09/23

--- 2Semestre/PHP/Formulario de Cadastro/consulta.php
+++ 2Semestre/PHP/Formulario de Cadastro/consulta.php
@@ -1,16 +1,16 @@
-<?PHP
-
-//$host = 'localhost';
-//$usuario = 'root';
-//$senha = '';
-//$banco = 'aluno';
+<?php
+//$servername = "nome_do_servidor";
+//$username = "nome_de_usuario";
+//$password = "senha";
+//$dbname = "nome_do_banco_de_dados";
 
 $conexao = mysqli_connect('localhost', 'root', '');
 $banco = mysqli_select_db($conexao, 'aluno');
 mysqli_set_charset($conexao, 'utf8');
-$sql = mysqli_query($conexao, "select * from aluno") or die ("Erro");
-while ($dados=mysqli_fecth_assoc($sql)) {
-	echo $dados['id'] . ' ';
-	echo $dados['nome'] . '<br>;'
+
+$sql = mysqli_query($conexao, "select * from aluno") or die("Erro.");
+while($dados = mysqli_fetch_assoc($sql)) {
+    echo $dados['id'] . ' ';
+    echo $dados['nome'] . '<br>';
 }
 ?>
--- 2Semestre/PHP/Formulario de Cadastro/process.php
+++ 2Semestre/PHP/Formulario de Cadastro/process.php
@@ -1,38 +1,30 @@
 <?php
-// Conexão com o banco de dados
-$host = 'localhost';
-$usuario = 'root';
-$senha = '';
-$banco = 'aluno';
-// Cria uma conexao com o banco de dados
-$conn = mysqli($host, $usuario, $senha, $banco);
-//Verifica se a conexao foi bem sucedida
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "aluno";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
-    // Redireciona para uma pagina de erro
-        header('Location: erro.php');
-        exit();
+    header("Location: erro.html");
+    exit();
 } else {
-    //Recupera os dados do formulario
     $nome = $_POST['nome'];
     $endereco = $_POST['endereco'];
     $email = $_POST['email'];
     $sexo = $_POST['sexo'];
-    $dataNasc = $_POST['dataNasc'];
-    //data em formato br para inserir no mysql
+    $dataNac = $_POST['dataNasc'];
     $data = implode("-", array_reverse(explode("/", $dataNasc)));
-    // Insere os dados na tabela do banco
-    $sql = "INSERT INTO aluno (nome,endereco, sexo, email,dataNasc) VALUES ('$nome','$endereco', $email', '$sexo', $dataNasc')";
+
+    $sql = "INSERT INTO aluno (nome, endereco, email, sexo, dataNasc)
+            VALUES ('$nome', '$endereco', '$email', '$sexo', '$dataNasc')";
+
     if ($conn->query($sql) === TRUE) {
-         //redireciona para uma pagina de sucesso
-        header('Location: sucesso.php');
+        header("Location: sucesso.html");
         exit();
     } else {
-        // Redireciona para uma pagina de erro
-        header('Location: erro.php');
-        exit();
-    } else {
-    // Redireciona para uma pagina de erro
-    header('Location: erro.php');
+        header("Location: erro.html");
         exit();
     }
 }

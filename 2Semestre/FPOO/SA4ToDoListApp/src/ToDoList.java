6d3314c2a4e2f999b780ce7d3f2561dd86ca181e
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2023-10-04 19:56:32 -0300

    Aula 04/10/23

---
+++ 2Semestre/FPOO/SA4ToDoListApp/src/ToDoList.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+import java.awt.*;
+
+public class ToDoList extends JFrame {
+    // atributos
+    private JPanel mainPanel;
+    private JTextField taskInputField;
+    private JButton addButton;
+    private JList<String> taskList;
+    private DefaultListModel<String> listModel;
+    private JButton deleteButton;
+    private JButton markDoneButton;
+    private JComboBox<String> filterComboBox;
+    private JButton clearCompletedButton;
+    private List<Task> tasks;
+
+    // construtor
+    public ToDoList() {
+        // Configuração da janela principal (JFrame)
+        super("To-Do List App");
+        this.setDefaultCloseOperation(2);
+        this.setBounds(100, 100, 400, 300);
+        // Inicializa o painel principal
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        // Inicializa a lista de tasks e a lista de tasks concluídas
+        tasks = new ArrayList<>();
+        listModel = new DefaultListModel<>();
+        taskList = new JList<>(listModel);
+        // Inicializa campos de entrada, botões e JComboBox
+        taskInputField = new JTextField();
+        addButton = new JButton("Adicionar");
+        deleteButton = new JButton("Excluir");
+        markDoneButton = new JButton("Concluir");
+        filterComboBox = new JComboBox<>(new String[] { "Todas", "Ativas", "Concluídas" });
+        clearCompletedButton = new JButton("Limpar Concluídas");
+        // Configuração do painel de entrada
+        JPanel inputPanel = new JPanel(new BorderLayout());
+        inputPanel.add(taskInputField, BorderLayout.CENTER);
+        inputPanel.add(addButton, BorderLayout.EAST);
+        // Configuração do painel de botões
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        buttonPanel.add(deleteButton);
+        buttonPanel.add(markDoneButton);
+        buttonPanel.add(filterComboBox);
+        buttonPanel.add(clearCompletedButton);
+        // Adiciona os componentes ao painel principal
+        mainPanel.add(inputPanel, BorderLayout.NORTH);
+        mainPanel.add(new JScrollPane(taskList), BorderLayout.CENTER);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+        // Adiciona o painel principal à janela
+        this.add(mainPanel);
+        this.setVisible(true);
+    }
+}

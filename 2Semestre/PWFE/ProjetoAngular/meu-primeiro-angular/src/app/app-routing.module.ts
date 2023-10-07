1eb10058864454aab1830650de45ae17940705da
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2023-10-06 21:52:48 -0300

    Aula 06/10/23

--- 2Semestre/PWFE/ProjetoAngular/meu-primeiro-angular/src/app/app-routing.module.ts
+++ 2Semestre/PWFE/ProjetoAngular/meu-primeiro-angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'contato', component: ContatoComponent},
+  {path: 'sobre', component: SobreComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

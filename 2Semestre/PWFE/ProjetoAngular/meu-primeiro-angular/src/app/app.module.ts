988d3bb09c338959c44529fb90ee9f0a92484e28
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2023-10-10 19:51:43 -0300

    Aula 10/10/23

--- 2Semestre/PWFE/ProjetoAngular/meu-primeiro-angular/src/app/app.module.ts
+++ 2Semestre/PWFE/ProjetoAngular/meu-primeiro-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { SobreComponent } from './sobre/sobre.component';
 import { ContatoComponent } from './contato/contato.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,13 @@ import { ContatoComponent } from './contato/contato.component';
     FooterComponent,
     HomeComponent,
     SobreComponent,
-    ContatoComponent
+    ContatoComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

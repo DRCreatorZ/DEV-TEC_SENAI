58f32d8bfe03b36289edeeab450dbd553e32d90e
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2023-10-10 21:55:16 -0300

    Aula 10/10/23

--- 2Semestre/PWFE/ProjetoVue/meu-projeto-vue/src/App.vue
+++ 2Semestre/PWFE/ProjetoVue/meu-projeto-vue/src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header>
+      <h1>Meu Catálogo de Filmes e Livros</h1>
+      <button @click="toggleView">Filmes/Livros</button>
+    </header>
+    <main>
+      <CatalogoDeFilmes v-if="showMovies" />
+      <CatalogoDeLivros v-else />
+    </main>
+    <footer>
+      <h1>D.R.C</h1>
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CatalogoDeFilmes from "@/components/CatalogoDeFilmes.vue";
+import CatalogoDeLivros from "./components/CatalogoDeLivros.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    CatalogoDeFilmes,
+    CatalogoDeLivros
+  },
+  data() {
+    return {
+      showMovies: true
+    };
+  },
+  methods: {
+    toggleView() {
+      this.showMovies = !this.showMovies;
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
+  <style>
+  #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  margin-top: 20px;
+  }
+  
+  header {
+  
+  background-color: #35495e;
+  color: #fff;
+  padding: 10px 0;
+  }
+
+  footer {
+  
+  background-color: #35495e;
+  color: #fff;
+  padding: 10px 0;
+  position: fixed;
+  left: 5px;
+  right: 5px;
+  bottom: 0px;
+  height: 50px;
+  margin-bottom: 5px;
+  }
+  </style>

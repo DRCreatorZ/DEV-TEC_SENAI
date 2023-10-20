8c99931b0f29e5ecbcef39a86ea5e6e3019682e3
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2023-10-19 21:58:47 -0300

    Aula 19/10/23

--- 2Semestre/PHP/Aula 17/Ex08/calcular.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'funcoes.php';
-
-$primos = encontrarPrimos(1000);
-
-echo "Números primos até 1000:<br>";
-echo implode(", ", $primos);
-?>
--- 2Semestre/PHP/Aula 17/Ex09/calcular.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'funcoes.php';
-
-$perfeitos = encontrarPerfeitos(1000);
-
-echo "Números perfeitos até 1000:<br>";
-echo implode(", ", $perfeitos);
-?>

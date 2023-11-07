7b023c90ee5cb287cba01059b1390cc622b20552
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2023-11-06 21:41:03 -0300

    Aula 06/11/23

---
+++ 2Semestre/FPOO/jdbcconnectionteste/src/main/java/Connection/ConnectionFactory.java
@@ -0,0 +1,33 @@
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    // atributos de conexão
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USUARIO = "postgres";
+    private static final String SENHA = "postgres";
+
+    // metodos de conexão
+    // get connection
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao obter conexão com o banco de dados.", e);
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ 2Semestre/FPOO/jdbcconnectionteste/src/main/java/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+import Connection.ConnectionDAO;
+
+public class Main {
+    public static void main(String[] args) {
+        new ConnectionDAO().apagarTabela();
+    }
+}

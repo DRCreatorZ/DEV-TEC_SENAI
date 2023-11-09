1dc5860f39eb6ef7317b2a5a101607334710ddf1
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2023-11-08 22:03:00 -0300

    Aula 08/11/23

--- 2Semestre/FPOO/guijdbclojacarros/src/main/java/connection/CarrosDAO.java
+++ 2Semestre/FPOO/guijdbclojacarros/src/main/java/connection/CarrosDAO.java
@@ -1,4 +1,4 @@
-package controller;
+package connection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import connection.ConnectionFactory;
+
 import model.Carros;
 
 /**
--- 2Semestre/FPOO/guijdbclojacarros/src/main/java/view/CarrosPainel.java
+++ 2Semestre/FPOO/guijdbclojacarros/src/main/java/view/CarrosPainel.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
+import connection.CarrosDAO;
+
 import java.awt.*;
 
 import model.Carros;
@@ -56,9 +58,25 @@ public class CarrosPainel extends JPanel {
         table = new JTable(tableModel);
         jSPane.setViewportView(table);
 
-        //tratamento de eventos(construtor)
+        // Cria o banco de dados caso não tenha sido criado
+        new CarrosDAO().criaTabela();
+        // incluindo elementos do banco na criação do painel
+        atualizarTabela();
+
+        // tratamento de eventos(construtor)
     }
 
-    //metodos de eventos (atualizar tabela)
+    // metodos de eventos (atualizar tabela)
+    // Método para atualizar a tabela de exibição com dados do banco de dados
+    private void atualizarTabela() {
+        tableModel.setRowCount(0); // Limpa todas as linhas existentes na tabela
+        carros = new CarrosDAO().listarTodos();
+        // Obtém os carros atualizados do banco de dados
+        for (Carros carro : carros) {
+            // Adiciona os dados de cada carro como uma nova linha na tabela Swing
+            tableModel.addRow(new Object[] { carro.getMarca(), carro.getModelo(),
+                carro.getAno(), carro.getPlaca(), carro.getValor() });
+        }
+    }
 
 }

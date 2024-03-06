1e584b9b2704dc0bdf1cd82b908abc4696287c44
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2024-03-06 19:54:11 -0300

    Aula 06/03/24

---
+++ 3Semestre/SpringBoot/contatojdbc/src/main/java/webapp/contatojdbc/ContatojdbcApplication.java
@@ -0,0 +1,13 @@
+package webapp.contatojdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatojdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatojdbcApplication.class, args);
+	}
+
+}
---
+++ 3Semestre/SpringBoot/contatojdbc/src/test/java/webapp/contatojdbc/ContatojdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp.contatojdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContatojdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

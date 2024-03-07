58e8376d3e95ee89901d3534dfb74a3644793222
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2024-03-06 21:53:26 -0300

    Aula 06/03/24

--- 3Semestre/SpringBoot/contatojdbc/src/main/java/webapp/contatojdbc/connection/ConnectionFactory.java
+++ 3Semestre/SpringBoot/contatojdbc/src/main/java/webapp/contatojdbc/connection/ConnectionFactory.java
@@ -1,5 +1,41 @@
 package webapp.contatojdbc.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
-    
+    private static final String url = "jdbc:postgresql://localhost:5432/DBnoite";
+    private static final String usuario = "postgres"; // Nome do ADM do banco
+    private static final String senha = "postgres"; // Senha do ADM do banco
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, usuario, senha);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao obter conexão com o banco de dados.", e);
+        }
+    }
+     public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    // Método para fechar a conexão e o objeto PreparedStatement
+    public static void closeConnection(Connection connection, PreparedStatement stmt) {
+        closeConnection(connection);
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
--- 3Semestre/SpringBoot/contatojdbc/src/main/java/webapp/contatojdbc/controller/IndexController.java
+++ 3Semestre/SpringBoot/contatojdbc/src/main/java/webapp/contatojdbc/controller/IndexController.java
@@ -1,5 +1,31 @@
 package webapp.contatojdbc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import webapp.contatojdbc.connection.EmailDAO;
+
+@Controller
 public class IndexController {
     
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView abrirIndex() {
+        ModelAndView mv = new ModelAndView("index");
+        EmailDAO obj = new EmailDAO();
+        String mensagem = "Olá, seja bem-vinda(o)!";
+        mv.addObject("msg", mensagem);
+        obj.criaTabela();
+        return mv;
+    }
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ModelAndView buscarIndex(@RequestParam("email") String email) {
+        ModelAndView mv = new ModelAndView("index");
+
+        new EmailDAO().cadastrar(email);
+
+        return mv;
+    }
 }

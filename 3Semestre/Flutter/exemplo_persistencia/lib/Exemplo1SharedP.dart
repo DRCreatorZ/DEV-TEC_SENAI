42ac1a7ecd925672519048cb30583fbc45638282
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2024-03-19 21:32:04 -0300

    Aula 19/03/24

---
+++ 3Semestre/Flutter/exemplo_persistencia/lib/Exemplo1SharedP.dart
@@ -0,0 +1,23 @@
+import 'package:exemplo_persistencia/main.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TaskListScreen(),
+    );
+  }
+}
+
+class TaskListScreen extends StatefulWidget {
+  _TaskListScreen createState() => _TaskListScreenState();
+}

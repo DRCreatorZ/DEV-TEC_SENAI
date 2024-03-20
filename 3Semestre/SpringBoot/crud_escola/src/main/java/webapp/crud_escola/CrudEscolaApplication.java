2b71a805aa58fc18268386fa1ba7123507749fe9
DRCreatorZ <111887448+DRCreatorZ@users.noreply.github.com>
2024-03-20 20:56:27 -0300

    Aula 20/03/24

---
+++ 3Semestre/SpringBoot/crud_escola/src/main/java/webapp/crud_escola/CrudEscolaApplication.java
@@ -0,0 +1,13 @@
+package webapp.crud_escola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEscolaApplication.class, args);
+	}
+
+}
---
+++ 3Semestre/SpringBoot/crud_escola/src/test/java/webapp/crud_escola/CrudEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp.crud_escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
